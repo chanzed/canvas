81cba4b0307f92409490c8ebd90cb7d5cc0c2986
zed <chanzedcz@gmail.com>
2018-09-28 02:17:05 +0800

    update eraser

--- main.js
+++ main.js
@@ -1,45 +1,77 @@
-cv = document.getElementById('xxx')
+var cv = document.getElementById('xxx')
+var ctx = cv.getContext('2d')
 setCanvasSize(cv)
+window.onresize = function () {
+  setCanvasSize(cv)
+}
+
+
 var using = false
 var lastPoint = {
   'x': undefined,
   'y': undefined
 }
 
-cv.onmousedown = function(yy){
-  using = true
-  lastPoint['x'] = yy.clientX
-  lastPoint['y'] = yy.clientY
-}
-
-cv.onmousemove = function(yy){
-  if(using){
+cv.onmousedown = function (yy) {
   var x = yy.clientX
   var y = yy.clientY
-  var ctx = cv.getContext('2d')
-  ctx.beginPath()
-  ctx.moveTo(lastPoint['x'], lastPoint['y'])
-  ctx.lineTo(x, y)
-  ctx.stroke()
-  lastPoint['x'] = x
-  lastPoint['y'] = y
+  if (eraserEnable) {
+    using = true
+    ctx.clearRect(x - 5, y - 5, 10, 10)
+  } else {
+    using = true
+    lastPoint = {
+      'x': x,
+      'y': y
+    }
   }
 }
 
-cv.onmouseup = function(){
+cv.onmousemove = function (yy) {
+  var x = yy.clientX
+  var y = yy.clientY
+  if (eraserEnable) {
+    if (using) {
+      ctx.clearRect(x - 5, y - 5, 10, 10)
+    }
+  } else {
+    if (using) {
+      drawLine(lastPoint['x'], lastPoint['y'], x, y)
+    }
+  }
+}
+
+cv.onmouseup = function () {
   using = false
 }
 
-
-
-
-
-window.onresize = function(){
-  setCanvasSize(cv)
-}
-function setCanvasSize(cv){
+function setCanvasSize(cv) {
   var pageWidth = document.documentElement.clientWidth
   var pageHeight = document.documentElement.clientHeight
   cv.width = pageWidth
-  cv.height = pageHeight 
+  cv.height = pageHeight
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.strokeStyle = 'green'
+  ctx.lineWidth = 3
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.stroke()
+  ctx.closePath()
+  lastPoint['x'] = x2
+  lastPoint['y'] = y2
+}
+
+var eraserEnable = false
+var eraser = document.getElementById('eraser')
+eraser.onclick = function () {
+  eraserEnable = true
+  action.className = 'action x'
+}
+
+brush.onclick = function () {
+  eraserEnable = false
+  action.className = 'action'
 }

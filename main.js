3876c054358b8a2493811c997941a51d0a429b3a
zed <chanzedcz@gmail.com>
2018-09-29 02:24:27 +0800

    改变代码的组织形式

--- main.js
+++ main.js
@@ -1,48 +1,60 @@
 var cv = document.getElementById('xxx')
 var ctx = cv.getContext('2d')
-setCanvasSize(cv)
-window.onresize = function () {
-  setCanvasSize(cv)
+
+autoSetCanvasSize(cv)
+listenToMouse(cv)
+var eraserEnable = false
+var eraser = document.getElementById('eraser')
+eraser.onclick = function () {
+  eraserEnable = true
+  action.className = 'action x'
 }
 
-
-var using = false
-var lastPoint = {
-  'x': undefined,
-  'y': undefined
+brush.onclick = function () {
+  eraserEnable = false
+  action.className = 'action'
 }
 
-cv.onmousedown = function (yy) {
-  var x = yy.clientX
-  var y = yy.clientY
-  if (eraserEnable) {
-    using = true
-    ctx.clearRect(x - 5, y - 5, 10, 10)
-  } else {
-    using = true
-    lastPoint = {
-      'x': x,
-      'y': y
-    }
+/************************** */
+
+function listenToMouse(canvas) {
+  var using = false
+  var lastPoint = {
+    'x': undefined,
+    'y': undefined
   }
-}
 
-cv.onmousemove = function (yy) {
-  var x = yy.clientX
-  var y = yy.clientY
-  if (eraserEnable) {
-    if (using) {
+  canvas.onmousedown = function (yy) {
+    var x = yy.clientX
+    var y = yy.clientY
+    if (eraserEnable) {
+      using = true
       ctx.clearRect(x - 5, y - 5, 10, 10)
-    }
-  } else {
-    if (using) {
-      drawLine(lastPoint['x'], lastPoint['y'], x, y)
+    } else {
+      using = true
+      lastPoint = {
+        'x': x,
+        'y': y
+      }
     }
   }
-}
 
-cv.onmouseup = function () {
-  using = false
+  canvas.onmousemove = function (yy) {
+    var x = yy.clientX
+    var y = yy.clientY
+    if (!using) { return }
+    if (eraserEnable) {
+      ctx.clearRect(x - 10, y - 10, 20, 20)
+    } else {
+      drawLine(lastPoint['x'], lastPoint['y'], x, y)
+      lastPoint['x'] = x
+      lastPoint['y'] = y
+    }
+  }
+
+  canvas.onmouseup = function () {
+    using = false
+  }
 }
 
 function setCanvasSize(cv) {
@@ -60,18 +72,14 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineTo(x2, y2)
   ctx.stroke()
   ctx.closePath()
-  lastPoint['x'] = x2
-  lastPoint['y'] = y2
+
 }
 
-var eraserEnable = false
-var eraser = document.getElementById('eraser')
-eraser.onclick = function () {
-  eraserEnable = true
-  action.className = 'action x'
-}
 
-brush.onclick = function () {
-  eraserEnable = false
-  action.className = 'action'
+
+function autoSetCanvasSize(canvas) {
+  setCanvasSize(canvas)
+  window.onresize = function () {
+    setCanvasSize(canvas)
+  }
 }

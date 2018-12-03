52f46b782031ecdb49d6cf21f625aeea00142152
zed <chanzedcz@gmail.com>
2018-12-04 00:47:50 +0800

    加了画笔粗细，颜色及下载功能1

--- main.js
+++ main.js
@@ -1,85 +1,177 @@
-var cv = document.getElementById('xxx')
-var ctx = cv.getContext('2d')
+var canvas = document.getElementById('canvas')
+var context = canvas.getContext('2d')
+var eraserEnabled = false
+var lineWidth = 5;
+autoSetCanvasSize(canvas)
+listenToUser(canvas)
 
-autoSetCanvasSize(cv)
-listenToMouse(cv)
-var eraserEnable = false
-var eraser = document.getElementById('eraser')
-eraser.onclick = function () {
-  eraserEnable = true
-  action.className = 'action x'
-}
+/******************************************************** */
 
-brush.onclick = function () {
-  eraserEnable = false
-  action.className = 'action'
-}
+function listenToUser(canvas) {
+    var using = false
+    var lastPoint = { x: undefined, y: undefined }
 
-/************************** */
-
-function listenToMouse(canvas) {
-  var using = false
-  var lastPoint = {
-    'x': undefined,
-    'y': undefined
-  }
-
-  canvas.onmousedown = function (yy) {
-    var x = yy.clientX
-    var y = yy.clientY
-    if (eraserEnable) {
-      using = true
-      ctx.clearRect(x - 5, y - 5, 10, 10)
+    //特性检测
+    if (document.body.ontouchstart !== undefined) {
+        canvas.ontouchstart = function (a) {
+            var x = a.touches[0].clientX
+            var y = a.touches[0].clientY
+            using = true
+            if (eraserEnabled) {
+                context.clearRect(x - 15, y - 15, 30, 30)
+            } else {
+                painting = true
+                lastPoint = { x: x, y: y }
+                drawCircle(x, y, 2)
+            }
+    
+        }
+    
+        //移动鼠标
+        canvas.ontouchmove = function (a) {
+            var x = a.touches[0].clientX
+            var y = a.touches[0].clientY
+            if (using) {
+                if (eraserEnabled) {
+                    context.clearRect(x - 15, y - 15, 30, 30)
+                } else {
+                    var newPoint = { x: x, y: y }
+                    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                    lastPoint = newPoint
+                }
+            }
+        }
+        //松开鼠标
+        canvas.ontouchend = function (a) {
+            using = false
+        }
     } else {
-      using = true
-      lastPoint = {
-        'x': x,
-        'y': y
-      }
+        canvas.onmousedown = function (a) {
+            var x = a.clientX
+            var y = a.clientY
+            using = true
+            if (eraserEnabled) {
+                context.clearRect(x - 15, y - 15, 30, 30)
+            } else {
+                painting = true
+                lastPoint = { x: x, y: y }
+                drawCircle(x, y, 2)
+            }
+    
+        }
+    
+        //移动鼠标
+        canvas.onmousemove = function (a) {
+            var x = a.clientX
+            var y = a.clientY
+            if (using) {
+                if (eraserEnabled) {
+                    context.clearRect(x - 15, y - 15, 30, 30)
+                } else {
+                    var newPoint = { x: x, y: y }
+                    drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                    lastPoint = newPoint
+                }
+            }
+        }
+        //松开鼠标
+        canvas.onmouseup = function (a) {
+            using = false
+        }
     }
-  }
-
-  canvas.onmousemove = function (yy) {
-    var x = yy.clientX
-    var y = yy.clientY
-    if (!using) { return }
-    if (eraserEnable) {
-      ctx.clearRect(x - 10, y - 10, 20, 20)
-    } else {
-      drawLine(lastPoint['x'], lastPoint['y'], x, y)
-      lastPoint['x'] = x
-      lastPoint['y'] = y
-    }
-  }
-
-  canvas.onmouseup = function () {
-    using = false
-  }
+    
 }
 
-function setCanvasSize(cv) {
-  var pageWidth = document.documentElement.clientWidth
-  var pageHeight = document.documentElement.clientHeight
-  cv.width = pageWidth
-  cv.height = pageHeight
-}
+//按下鼠标
+
 
 function drawLine(x1, y1, x2, y2) {
-  ctx.strokeStyle = 'green'
-  ctx.lineWidth = 3
-  ctx.beginPath()
-  ctx.moveTo(x1, y1)
-  ctx.lineTo(x2, y2)
-  ctx.stroke()
-  ctx.closePath()
-
+    context.beginPath()
+    context.lineWidth = lineWidth
+    context.moveTo(x1, y1)
+    context.lineTo(x2, y2)
+    context.stroke()
+    context.closePath()
 }
+
+function drawCircle(x, y, radius) {
+    context.beginPath()
+    context.arc(x, y, radius, 0, Math.PI * 2)
+    context.fill()
+    context.closePath()
+}
+
+function setCanvasSize(canvas) {
+    var pageWidth = document.documentElement.clientWidth
+    var pageHight = document.documentElement.clientHeight
+    canvas.width = pageWidth
+    canvas.height = pageHight
+}
+
 
 
 
 function autoSetCanvasSize(canvas) {
-  setCanvasSize(canvas)
-  window.onresize = function () {
     setCanvasSize(canvas)
-  }
+    window.onresize = function () {
+        setCanvasSize(canvas)
+    }
+}
+
+pen.onclick = function(){
+    console.log('点击pen')
+    eraserEnabled = false
+    pen.classList.add('active')
+    eraser.classList.remove('active')
+}
+eraser.onclick = function(){
+    console.log('点击eraser')
+    eraserEnabled = true
+    eraser.classList.add('active')
+    pen.classList.remove('active')
+}
+
+red.onclick = function() {
+    context.strokeStyle = 'red'
+    context.fillStyle = 'red'
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+green.onclick = function() {
+    context.strokeStyle = 'green'
+    context.fillStyle = 'green'
+    green.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+}
+blue.onclick = function() {
+    context.strokeStyle = 'blue'
+    context.fillStyle = 'blue'
+    blue.classList.add('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
+}
+
+thin.onclick = function() {
+    lineWidth = 5;
+}
+
+thick.onclick = function() {
+    lineWidth = 8;
+}
+
+clear.onclick = function() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+download.onclick = function() {
+    var url = canvas.toDataURL("image/png")
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url;
+    a.download = '我的画儿'
+    a.target = '_blank'
+    a.click()
+
 }
